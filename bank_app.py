8f1071fac40a862ed8bd55381dbd806032ce8f14
Bongani <phalarichard1@gmail.com>
2022-09-15 14:17:35 +0200

    Working on inputs and spacing for app

--- bank_app.py
+++ bank_app.py
@@ -3,6 +3,7 @@ import random as r
 #User Class Here: 
 class User():
     #Constructor
+    print("Welcome to King Bank (PTY)Ltd\n")
     def __init__(self,name,age,gender,tel_num,address):
         self.name = name
         self.age = age
@@ -10,22 +11,31 @@ class User():
         self.tel_num = tel_num
         self.address = address
         
+    #Bank Registration Here:
+    def account_holder(self):
+        print('PLEASE BEGIN REGISTRATION:\n')
+        self.name = input('Enter your name:\n ')
+        self.age = input('Enter your age:\n ')
+        self.gender = input('Enter your Gender:\n ')
+        self.tel_num = input('Enter your Contact Number:\n ')
+        self.address = input('Enter your Home Addess:\n ')
+        print(f'\nHello there, {self.name}, Welcome to King Bank. Your balance is R0.00 please make a deposit ')
+        
     #Checking if person is employee
     def user_details(self):
         print(f"Thank you, {self.name} with tel {self.tel_num} for choosing us")
         
 #Bank Account Class Here:      
 class BankAccount(User):
-    acc_numb = 2799758187
+    acc_numb = 0
     
     def __init__(self,name,age,gender,tel_num,address,initial_deposit,pin):
         super().__init__(name,age,gender,tel_num,address)
         self.acc_balance = initial_deposit
         self.acc_num = BankAccount.acc_numb
         self.pin = pin
-        print('Account details is saved, thank you')
         
-        BankAccount.acc_numb = BankAccount.acc_numb + 1
+        BankAccount.acc_numb = BankAccount.acc_numb + r.randrange(999999999999)
     
     def show_details(self):
         print(f'Hi {self.name}\t with Account Number: {self.acc_numb}\t Your Current Balance is R{self.acc_balance}')
@@ -41,7 +51,8 @@ class BankAccount(User):
 #Creating an Object
 if __name__ == '__main__':
     
-    customer1 = BankAccount(name = 'Bongani',age= '33',gender = 'Male',tel_num='0681783394',address='110 Pine Ave Centurion', initial_deposit= 1500, pin= 1988)
+    customer1 = BankAccount(name ='',age= '',gender = '',tel_num='0681783394',address='110 Pine Ave Centurion', initial_deposit= 1500, pin= 1988)
+    customer1.account_holder()
     print(customer1.show_details())
     customer1.deposit()
     
