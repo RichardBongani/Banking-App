c461e1b6676aca9e4057f24e090b9ab4e951a19c
Bongani <phalarichard1@gmail.com>
2022-09-21 16:48:32 +0200

    Working on Home Page and Views

--- authbanking/usersapp/views.py
+++ authbanking/usersapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse ('<h2>Welcome to King Bank</h2>')
+    return render(request, 'users/home.html')
